f840e539b75c00c5613c32f745865d4a7aa594f7
FredAnderssen <Diriko1997@live.com>
2017-12-01 12:54:55 +0100

    Create DivLibWORK1.h
    DivLibWORK1.h

---
+++ DivLibWORK1.h
@@ -0,0 +1,118 @@
+#pragma once
+/*
+* File: DivLib.h
+* Version: 1.0
+* Last modified on Sat Nov 18 2017 by AnFr16lp
+* -----------------------------------------------------
+* This interface provides several functions for user input
+*
+*/
+
+#ifndef _DivLib_h
+#define _DivLib_h
+
+#include <stdio.h>
+#include "genlib.h"
+
+#define SIZE 4
+/* Function prototypes */
+
+/************************************************************************************
+* This function takes a string and try finding the file with that name and
+* prints out the txtfile.
+************************************************************************************/
+void printTxtFile(char* filename);
+
+/************************************************************************************
+* This function takes a string and try finding the file with that name and
+* prints out the txtfile.
+************************************************************************************/
+void prntMenu(void);
+
+/************************************************************************************
+* This function takes a string and try finding the file with that name and
+* prints out the txtfile.
+************************************************************************************/
+void Menu(int);
+
+/************************************************************************************
+* This function takes a string and try finding the file with that name and
+* prints out the txtfile.
+************************************************************************************/
+void getMatrix(void);
+
+/************************************************************************************
+* This function takes a string and try finding the file with that name and
+* prints out the txtfile.
+************************************************************************************/
+void prntMatrix(int [][SIZE]);
+
+/************************************************************************************
+* This function takes a string and try finding the file with that name and
+* prints out the txtfile.
+************************************************************************************/
+bool diaTest(int, int, int);
+
+/************************************************************************************
+* This function takes a string and try finding the file with that name and
+* prints out the txtfile.
+************************************************************************************/
+bool rowTest(int, int, int[][SIZE]);
+
+/************************************************************************************
+* This function takes a string and try finding the file with that name and
+* prints out the txtfile.
+************************************************************************************/
+bool colTest(int, int, int[][SIZE]);
+
+/************************************************************************************
+* This function takes a string and try finding the file with that name and
+* prints out the txtfile.
+************************************************************************************/
+
+/************************************************************************************
+* This function takes a string and try finding the file with that name and
+* prints out the txtfile.
+************************************************************************************/
+
+/************************************************************************************
+* This function takes a string and try finding the file with that name and
+* prints out the txtfile.
+************************************************************************************/
+
+/************************************************************************************
+* This function takes a string and try finding the file with that name and
+* prints out the txtfile.
+************************************************************************************/
+
+/************************************************************************************
+* This function takes a string and try finding the file with that name and
+* prints out the txtfile.
+************************************************************************************/
+
+/************************************************************************************
+* This function takes a string and try finding the file with that name and
+* prints out the txtfile.
+************************************************************************************/
+
+/************************************************************************************
+* This function takes a string and try finding the file with that name and
+* prints out the txtfile.
+************************************************************************************/
+
+/************************************************************************************
+* This function takes a string and try finding the file with that name and
+* prints out the txtfile.
+************************************************************************************/
+
+/************************************************************************************
+* This function takes a string and try finding the file with that name and
+* prints out the txtfile.
+************************************************************************************/
+
+/************************************************************************************
+* This function takes a string and try finding the file with that name and
+* prints out the txtfile.
+************************************************************************************/
+
+#endif
